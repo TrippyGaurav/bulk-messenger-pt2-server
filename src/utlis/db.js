d342e01b18652b89c077e22d834605cb37b93593
TrippyGaurav <gaurav.trippybug@gmail.com>
2024-03-11 09:14:04 +0530

    add env

--- src/utlis/db.js
+++ src/utlis/db.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "9870",
-  host: "localhost",
-  database: "messenger",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
 });
 
 module.exports = pool;
