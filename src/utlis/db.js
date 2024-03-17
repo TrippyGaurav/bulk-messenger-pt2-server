de24128b3b6595ebb8cbcabe52ab9e2eb0ec6b46
MohitNegi007 <mohitnegi45801@gmail.com>
2024-03-17 22:46:55 +0530

    fixed db encrption error

--- src/utlis/db.js
+++ src/utlis/db.js
@@ -3,13 +3,26 @@ const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
 
+//Pro
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
   host: process.env.DB_HOST,
-  ssl: true, // If you're in production, enable SSL, otherwise, disable it
+});
+
+// Dev-DB
+// const pool = new Pool({
+//   user: process.env.DB_DEV_USER,
+//   password: process.env.DB_DEV_PASSWORD,
+//   database: process.env.DB_DEV_DATABASE,
+//   port: 5432,
+//   host: process.env.DB_DEV_HOST,
+// });
+
+pool.on("connect", () => {
+  console.log("Database connected");
 });
 
 module.exports = { pool };
