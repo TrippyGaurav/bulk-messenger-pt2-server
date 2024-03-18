0403c418fb5a9dc5d4cfb4a60768d0411fec8574
MohitNegi007 <mohitnegi45801@gmail.com>
2024-03-18 14:38:55 +0530

    dockerise app

--- src/controllers/messengerController.js
+++ src/controllers/messengerController.js
@@ -40,7 +40,18 @@ const sendMessage = async (req, res) => {
 
     let browser;
     try {
-      browser = await puppeteer.launch({ headless: false });
+      browser = await puppeteer.launch({
+        args: [
+          "--disable-setuid-sandbox",
+          "--no-sandbox",
+          "--single-process",
+          "--no-zygote",
+        ],
+        executablePath:
+          process.env.NODE_ENV === "production"
+            ? process.env.PUPPETEER_EXECUTABLE_PATH
+            : puppeteer.executablePath(),
+      });
       const page = await browser.newPage();
 
       // Navigate to Facebook login Page
